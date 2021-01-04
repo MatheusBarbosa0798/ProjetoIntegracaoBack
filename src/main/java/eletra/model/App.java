f622e75ec5946a272f4a3db13eb65fa271f1e90e
Matheus Barbosa <matheusbarbosa0798@gmail.com>
2021-01-04 14:14:42 -0300

    main app in backend ready to "delete" method

--- src/main/java/eletra/model/App.java
+++ src/main/java/eletra/model/App.java
@@ -7,8 +7,10 @@ public class App {
 	
     public static void main(String[] args) {
     	 SystemService systemService = new SystemService();
-         System.out.println("*** Service - start ***");    
-         systemService.delete(11);
+         System.out.println("*** Service - start ***");  
+         System.out.println("Number of products is now " + systemService.findAll().size() + ".");
+         systemService.delete(0);
+         System.out.println("*** Delete - complete ***");
          System.out.println("Number of products is now " + systemService.findAll().size() + ".");
          System.exit(0);
     }
